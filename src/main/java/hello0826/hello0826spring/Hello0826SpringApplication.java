5759c270d7069c60a43ff911fcb30bba2ab3ab8f
psh <psh263090@gmail.com>
2023-08-26 18:01:28 +0900

    start

---
+++ src/main/java/hello0826/hello0826spring/Hello0826SpringApplication.java
@@ -0,0 +1,13 @@
+package hello0826.hello0826spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello0826SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hello0826SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello0826/hello0826spring/Hello0826SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello0826.hello0826spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello0826SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
